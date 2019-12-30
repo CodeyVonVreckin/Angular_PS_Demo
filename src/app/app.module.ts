e85a4af961b4227d48b5b4d270c2992c2ef71b21
CodeyVonVreckin <jvonvreckin@ups.com>
2019-12-30 11:51:18 -0500

    added pipes to a few table elements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import { StarComponent } from './shared/start.component';
 
 @NgModule({
+  //  these are for componets i make
   declarations: [
     AppComponent,
     ProductListComponent,
     StarComponent
   ],
+  //  3rd party components/modules
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   bootstrap: [AppComponent]
 })
